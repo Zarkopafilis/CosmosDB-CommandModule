61b10f1eac02f517ceb69632b1c14c4ddb0b731c
Nick Chapsas <elfocrash@gmail.com>
2018-11-03 16:59:35 +0000

    Initial commit

---
+++ CosmosDBCommandModule/CosmosDBCommandModule.Server/Controllers/CosmosDbController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CosmosDBCommandModule.Server.Controllers
+{
+    [Route("api")]
+    public class CosmosDbController : Controller
+    {
+
+    }
+}
